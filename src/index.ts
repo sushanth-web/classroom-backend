0d5057b7d6d00927958f6447c13e56ee73d44a3e
sushanth-web <sushanthweb@gmail.com>
2026-06-20 12:48:59 +0530

    implement add all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import subjectsRouter from "./routes/subjects.js"
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    methods: ['GET','POST','PUT','DELETE'],
+    credentials: true
+}))
+
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter);
 
 app.get('/', (req,res) => {
     res.send('Hello, Welcome to the Classroom API!');
